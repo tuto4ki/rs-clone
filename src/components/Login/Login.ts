86352dde9f3520cc8e153a8b7ab75e20c18494ae
VladimirG91 <vladimir.glazkov91@gmail.com>
2023-02-05 19:56:44 +0600

    fix: form validation

--- src/components/Login/Login.ts
+++ src/components/Login/Login.ts
@@ -3,167 +3,92 @@ import './style.css';
 
 export default class Login {
   private _headline: HTMLParagraphElement;
-  private _passwordFiled: InputField;
-  private _loginFiled: InputField;
+  private _passwordField: InputField;
+  private _loginField: InputField;
+  private _loginState: boolean;
+  private _button: HTMLButtonElement;
+  private _form: HTMLDivElement;
 
   constructor() {
-    this._headline = elementGenerator.createParagraph({ className: 'headline', text: 'Login' });
-    this._passwordFiled = new InputField('password', '', 'pwd', 'Password', 'Enter your password');
-    this._loginFiled = new InputField('text', '', 'user-name', 'Username', 'Enter username');
+    this._loginState = true;
+    this._loginField = new InputField('text', '', 'user-name', 'Username', 'Enter username');
+    this._passwordField = new InputField('password', '', 'pwd', 'Password', 'Enter your password');
+
+    this._headline = elementGenerator.createParagraph({
+      className: 'headline',
+    });
+    this._button = elementGenerator.createButton({
+      className: 'button',
+    });
+    this._form = elementGenerator.createDiv({ className: 'form' });
+    this._button.addEventListener('click', () => {
+      //
+      this.validateInputs();
+    });
+  }
+
+  private changeFormState() {
+    this._loginState = !this._loginState;
+    this.setFieldText();
+  }
+
+  // buttonPressed() {
+  //   if (this._loginState) {
+  //     //logging
+  //   } else {
+  //     //registration
+  //   }
+  // }
+
+  private setFieldText() {
+    this._headline.innerText = `${this._loginState ? 'Login' : 'Register'}`;
+    this._button.innerHTML = `${this._loginState ? 'Login' : 'Register'}`;
   }
 
   setTranslation() {
+    this.setFieldText();
     this._headline.innerText = 'iz massiva text';
-    this._passwordFiled.setTranslation();
-    this._loginFiled.setTranslation();
+    this._loginField.setTranslation();
+    this._passwordField.setTranslation();
     // TODO
   }
 
-  createForm() {
-    // const root = document.querySelector('#root') as HTMLDivElement;
-    const fragment = document.createDocumentFragment();
-    const forms = elementGenerator.createDiv({ className: 'forms' });
-    // forms.classList.add('forms');
-    // root.append(forms);
-    const formLoginContainer = elementGenerator.createDiv({ className: 'formLoginContainer' });
-    // formLoginContainer.classList.add('formLoginContainer');
-    // const formLogin = document.createElement('form');
-    // formLogin.action = '#';
-    // formLogin.classList.add('formLogin', 'form');
-    // formLogin.id = 'formLogin';
-    formLoginContainer.append(this._headline, this._passwordFiled.getInputField(), this._loginFiled.getInputField());
-    fragment.append(formLoginContainer);
-    forms.append(fragment);
-    // const formLoginHeader = document.createElement('h1');
-    // this._headline.textContent = 'Login';
-
-    // formLoginContainer.append(formLogin);
-    //
-    const inputLoginControl = document.createElement('div');
-    inputLoginControl.classList.add('input-control');
-    const labelLogin = document.createElement('label');
-    labelLogin.textContent = 'Username';
-    labelLogin.htmlFor = 'login';
-    const login = document.createElement('input');
-    login.id = 'login';
-    login.type = 'text';
-    login.placeholder = 'Enter Your Username';
-    const err1 = document.createElement('div');
-    err1.classList.add('error');
-    inputLoginControl.append(labelLogin, login, err1);
-    // formLogin.append(inputLoginControl);
-    //
-    const inputPasswordControl = document.createElement('div');
-    inputPasswordControl.classList.add('input-control');
-    const labelPassword = document.createElement('label');
-    labelPassword.textContent = 'Password';
-    labelPassword.htmlFor = 'password';
-    const password = document.createElement('input');
-    password.id = 'password';
-    password.type = 'password';
-    password.placeholder = 'Enter Your Password';
-    const err2 = document.createElement('div');
-    err2.classList.add('error');
-    inputPasswordControl.append(labelPassword, password, err2);
-    // formLogin.append(inputPasswordControl);
-    //
-    const btn = document.createElement('button');
-    btn.textContent = 'Log In Now';
-    btn.type = 'submit';
-    // formLogin.append(btn);
-    //
-    const registrationContainer = document.createElement('div');
-    registrationContainer.classList.add('registrationContainer');
-    const spanSign = document.createElement('span');
-    spanSign.classList.add('spanSign');
-    spanSign.textContent = 'You don`t have account?';
-    const signUpLink = document.createElement('a');
-    signUpLink.href = '#';
-    signUpLink.textContent = 'Register Now';
-    signUpLink.classList.add('registrationLink');
-    registrationContainer.append(spanSign, signUpLink);
-    // formLogin.append(registrationContainer);
-
-    //
-    const formRegistrationContainer = document.createElement('div');
-    formRegistrationContainer.classList.add('formRegistrationContainer');
-    const formRegistration = document.createElement('form');
-    formRegistration.action = '#';
-    formRegistration.classList.add('formRegistration', 'form');
-    formRegistration.id = 'formRegistration';
-    forms.append(formRegistrationContainer);
-    const formRegistrationHeader = document.createElement('h1');
-    formRegistrationHeader.textContent = 'Registration';
-    formRegistrationContainer.append(formRegistrationHeader);
-    //
-    const inputNameControl = document.createElement('div');
-    inputNameControl.classList.add('input-control');
-    const labelUser = document.createElement('label');
-    labelUser.textContent = 'Username';
-    labelUser.htmlFor = 'login';
-    const username = document.createElement('input');
-    username.id = 'username';
-    username.type = 'text';
-    username.placeholder = 'Enter Your Username';
-    const err3 = document.createElement('div');
-    err3.classList.add('error');
-    inputNameControl.append(labelUser, username, err3);
-    formRegistrationContainer.append(formRegistration);
-    formRegistration.append(inputNameControl);
-    //
-    const inputPswControl = document.createElement('div');
-    inputPswControl.classList.add('input-control');
-    const labelPsw = document.createElement('label');
-    labelPsw.textContent = 'Password';
-    labelPsw.htmlFor = 'password';
-    const psw = document.createElement('input');
-    psw.id = 'psw';
-    psw.type = 'password';
-    psw.placeholder = 'Create Your Password';
-    const err4 = document.createElement('div');
-    err4.classList.add('error');
-    inputPswControl.append(labelPsw, psw, err4);
-    formRegistration.append(inputPswControl);
-    //
-    const inputEmail = document.createElement('div');
-    inputEmail.classList.add('input-control');
-    const labelEmail = document.createElement('label');
-    labelEmail.textContent = 'Email';
-    labelEmail.htmlFor = 'password';
-    const email = document.createElement('input');
-    email.id = 'email';
-    email.type = 'email';
-    email.placeholder = 'Enter Your Email';
-    const err5 = document.createElement('div');
-    err5.classList.add('error');
-    inputEmail.append(labelEmail, email, err5);
-    formRegistration.append(inputEmail);
-    //
-    const btnReg = document.createElement('button');
-    btnReg.textContent = 'Register Now';
-    btnReg.type = 'submit';
-    formRegistration.append(btnReg);
-    //
-    const loginContainer = document.createElement('div');
-    loginContainer.classList.add('loginContainer');
-    const spanSign1 = document.createElement('span');
-    spanSign1.classList.add('spanSign1');
-    spanSign1.textContent = 'Already have an account?';
-    const loginLink = document.createElement('a');
-    loginLink.href = '#';
-    loginLink.textContent = 'Log In';
-    loginLink.classList.add('registrationLink');
-    loginContainer.append(spanSign1, loginLink);
-    formRegistration.append(loginContainer);
-
+  createForm(): HTMLDivElement {
+    this.setFieldText();
+    this._form.append(
+      this._headline,
+      this._loginField.getInputField(),
+      this._passwordField.getInputField(),
+      this._button
+    );
     console.log('a');
-    return forms;
+    return this._form;
+  }
+
+  private validateInputs() {
+    //
+    console.log('validate');
+
+    if (this._loginField.getInputValue() === '') {
+      this._loginField.setErrorMessage();
+    } else {
+      this._loginField.setSuccessMessage();
+    }
+
+    if (this._passwordField.getInputValue() === '') {
+      this._passwordField.setErrorMessage();
+    } else if (this._passwordField.getInputValue().length < 8) {
+      this._passwordField.setErrorMessage();
+    } else {
+      this._passwordField.setSuccessMessage();
+    }
   }
 }
 
 class InputField {
   private _inputField: HTMLInputElement;
   private _label: HTMLLabelElement;
+  private _field: HTMLDivElement;
   constructor(
     inputType: string,
     inputClassName: string,
@@ -177,15 +102,38 @@ class InputField {
       id: inputId,
     });
     this._label = elementGenerator.createLabel({ for: inputId, text: labelValue });
+    this._field = elementGenerator.createDiv({ className: 'input-wrap' });
   }
 
-  getInputField() {
-    const field = elementGenerator.createDiv({ className: 'input-wrap' });
-    field.append(this._label, this._inputField);
-    return field;
+  getInputField(): HTMLDivElement {
+    this._field.append(this._label, this._inputField);
+    return this._field;
+  }
+
+  getInputValue(): string {
+    return this._inputField.value;
+  }
+
+  setErrorMessage() {
+    this._inputField.classList.add('error');
+    this._inputField.classList.remove('success');
+  }
+
+  setSuccessMessage() {
+    this._inputField.classList.remove('error');
+    this._inputField.classList.add('success');
   }
 
   setTranslation() {
     // set translation for inputs TODO
   }
+
+  // validateInputs(): void {
+  //   //
+  //   if (this._inputField.value === '') {
+  //     this.setErrorMessage();
+  //   } else {
+  //     this.setSuccessMessage();
+  //   }
+  // }
 }
--- src/components/Login/index.ts
+++
@@ -1,7 +0,0 @@
-// import Login from './login';
-// import validateForm from './validation';
-// import './style.css';
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-// const control = new Login();
-// validateForm();
